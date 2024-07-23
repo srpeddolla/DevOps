7aaa2f00364e7a434ec05b9329540fcc61436913
srpeddolla <shashidhar.peddolla@outlook.com>
2024-07-23 23:31:19 +0530

    commit

--- Terraform/terraform-workshop/main.tf
+++
@@ -1,5 +0,0 @@
-resource "github_repository" "my_first_repo_using_repository" {
-  name = "my_first_repo_using_repository"
-  description = "github repository using terraform"
-  visibility = "public" # Public repository
-}
