7762e937d796f68b4ecb82cce1f9a11c1746a8d4
srpeddolla <shashidhar.peddolla@outlook.com>
2024-07-28 00:08:23 +0530

    committing changes to terraform 3 tier

--- terraform_3_tier/output.tf
+++ terraform_3_tier/output.tf
@@ -1,8 +1,4 @@
 output "lb_dns_name" {
   description = "arn:aws:elasticloadbalancing:us-east-1:009160067511:loadbalancer/app/tf-lb-20240726062316534800000001/6c144c06427ff01f"
-<<<<<<< HEAD
   value = ""
-=======
-  value = "aws_lb.mylistener.dns_mylistener-1078043237.us-east-1.elb.amazonaws.com"
->>>>>>> 253c946886f50633ca6dae85941059457d76d2a2
 }
