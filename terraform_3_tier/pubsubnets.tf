51a4431121e5b9b56a6b2d3575c4912553509c3c
srpeddolla <shashidhar.peddolla@outlook.com>
2024-07-27 13:47:03 +0530

    commit

--- terraform_3_tier/pubsubnets.tf
+++ terraform_3_tier/pubsubnets.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public-1" {
   vpc_id = aws_vpc.vpc1.id
   map_public_ip_on_launch = true
   availability_zone = "us-east-1b"
-  cidr_block = "10.0.1.0/16"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     "name" = "public-1"
@@ -13,7 +13,7 @@ resource "aws_subnet" "public-2" {
   vpc_id = aws_vpc.vpc1.id
   map_public_ip_on_launch = true
   availability_zone = "us-east-1b"
-  cidr_block = "10.0.2.0/16"
+  cidr_block = "10.0.2.0/24"
 
   tags = {
     "name" = "public-2"
