24fff8fdf3cad20cfa6766f26cbd77c65d3bf1be
srpeddolla <shashidhar.peddolla@outlook.com>
2024-08-18 22:05:34 +0530

    updated new scripts

---
+++ Linux/forloop.sh
@@ -0,0 +1,3 @@
+# iterations for loop, while loop, until loop, select loop etc.
+
+for i in {1.100}; do echo $1; done
---
+++ Linux/ifelse.sh
@@ -0,0 +1,10 @@
+a=4
+b=10
+
+if [ $a > $b ]
+
+then
+    echo "a is greater than b"
+else
+    echo "b is greater than a"
+fi
